c589987b3147352cf9669555f4923f93595f0068
fadeonsp <edfadeon@gmail.com>
2023-03-31 19:13:07 -0300

    envinado para github

---
+++ src/components/Input/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #aaffaa;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #aaffaa;
+        padding: 0 10px;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        float: right;
+        clear: both;
+        font-size: 24px;
+    }
+`
